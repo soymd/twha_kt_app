bad5de33a46661fd127b3a245cc2c66713fdd709
soymd <pacificstate33@gmail.com>
2020-03-30 15:01:40 +0900

    ・3・

--- app/src/main/java/com/example/twhaKtApp/view/ui/main/TwhaStatsActivity.kt
+++ app/src/main/java/com/example/twhaKtApp/view/ui/main/TwhaStatsActivity.kt
@@ -3,22 +3,11 @@ package com.example.twhaKtApp.view.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.twhaKtApp.R
-import com.example.twhaKtApp.TwhaRepository
 
 class TwhaStatsActivity : AppCompatActivity() {
-
-    private var userId = 1;
-
-    private val repository = TwhaRepository()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_twha_stats)
-
-        repository.readAnswers(this, userId)
-
-
         supportFragmentManager.beginTransaction().replace(
             R.id.container_main_fragment,
             StatsFragment()
